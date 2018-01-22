5c3240ed8d2ba2460be055765421fc60ac978c02
HBN <ngohoangbao@outlook.com>
2018-01-22 17:23:47 +0100

    Finito, Dungeon Crawler Optimized version, not the React Way.
    Could probably code better in the first version, it was after I completed the game I noticed that the massive overhead I created by updating every changes I made.
    Many of them could be fixed by adding componentShouldUpdate and spreading/split the states into smaller classes thus making sure only updating on specific component.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
-// import { Monster, Items, Weapons, Player, Boss, Wall, Road, Cell } from "./scripts/entities";
 import { Cell } from "./scripts/entities";
-
 
 // Game Configurations
 var gRooms = 10, cells = [], fog = false;
-// var fog = true;
 var yLength = 60, xLength = 100;
 var totalEntities = {
     "red": 1,      //player
@@ -17,68 +14,14 @@ var totalEntities = {
     "blue": 1         //boss
 };
 
-
 // Dungeon and player informaiton
 var expcap = 10, playerPosition = 0;
 var health = 50, level = 1, exp = 0, weapon = 2, dungeon = 1;
 
 // Should optimize the program, takes too much CPU resources
 class Main extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = this.Initialstate;
-    }
-
-    get Initialstate(){
-        return {dungeon: []};
-    }
-
-    resetMap = (bool) => {
-        if (this.state.lvlDungeon === 20) {
-            this.setState(this.Initialstate, function validate() {
-                this.setState({ dungeon: this.createMap(10) }, this.showMessage(true, true));
-            });
-        } else
-            if (bool) {
-                // Won
-                var hp = this.state.health, lvl = this.state.level, xp = this.state.exp, ecap = this.state.expcap, wp = this.state.weapon, dlvl = this.state.lvlDungeon;
-                this.setState(this.Initialstate, function validate() {
-                    this.setState({
-                        dungeon: this.createMap(gRooms++),
-                        health: hp,
-                        level: lvl,
-                        exp: xp,
-                        expcap: ecap,
-                        weapon: wp,
-                        lvlDungeon: dlvl + 1
-                    });
-                });
-                this.showMessage(true, false);
-            } else {
-                // Dead, Dark souls difficulty, you start at dungeon level 1 again peasant!
-                // var hp = this.state.health, lvl = this.state.level, wp = this.state.weapon, dlvl = this.state.lvlDungeon;
-                this.setState(this.Initialstate, function validate() {
-                    this.setState({ dungeon: this.createMap(10) }, this.showMessage(false, false));
-                });
-            }
-    }
-
-    showMessage = (bool, won) => {
-        if (bool && won) {
-            console.log("Won the game");
-        } else if (bool) {
-            console.log("Won dungeon");
-            gRooms++;
-        } else {
-            console.log("Dead");
-            gRooms = 10;
-        }
-    }
-
-   
     // Creating rooms
     createRoom(position, sizeX, sizeY, available) {
-        // this.refs[xLength * (position.row) + (position.column)].setEntity("white");
         for (var x = 0; x <= sizeX; x++) {
             for (var y = 0; y <= sizeY; y++) {
                 this.refs[xLength * (position.row + x) + (position.column + y)].setEntity("white");
@@ -87,29 +30,16 @@ class Main extends React.Component {
         }
     }
 
-    // Get a random vacant location on the map
-    getRandomPos(available) {
-        return available.splice(Math.floor(Math.random() * available.length), 1);
-    }
-
-
-    // To many states to handle at the same time?
     populateDungeon(available) {
-        // var array = this.state.elements.slice();
-        var occupied = [], current = 0, tmp = {};
-
         var keys = Object.keys(totalEntities);
-        console.log(keys.length);
 
         for(var i = 0; i < keys.length; i++){
-            // console.log(keys[i]);
             for(var y = 0; y < totalEntities[keys[i]]; y++){
                 var pos =  available.splice(Math.floor(Math.random() * available.length), 1);
                 if(keys[i] === "red"){
                     this.refs[pos].setEntity(keys[i], 0, 0);             //player
                     this.refs[pos].fog();
                     playerPosition = pos;
-                    
                 }
                 else if(keys[i] === "green") this.refs[pos].setEntity(keys[i], Math.floor(Math.random() * dungeon + 5),  Math.floor(Math.random() * dungeon + 5));      //monsters
                 else if(keys[i] === "blue") this.refs[pos].setEntity(keys[i], Math.floor(Math.random() * dungeon + 10),  Math.floor(Math.random() * dungeon + 10));       //boss
@@ -117,7 +47,6 @@ class Main extends React.Component {
                 else if(keys[i] === "purple") this.refs[pos].setEntity(keys[i], Math.floor(Math.random() * dungeon + 20), 0);     //items
             }
         }
-       
     }
 
     // Create bindings/tunnels to each room
@@ -144,7 +73,7 @@ class Main extends React.Component {
 
         while (loop--) {
             let sizeX = Math.floor(Math.random() * 12 + 1), sizeY = Math.floor(Math.random() * 12 + 1);
-            let position = { column: Math.floor(Math.random() * (xLength - 15) + 2), row: Math.floor(Math.random() * (yLength - 15) + 2) }; // dont start around corners
+            let position = { column: Math.floor(Math.random() * (xLength - 15) + 2), row: Math.floor(Math.random() * (yLength - 15) + 2) }; // dont start around corners nor edges
             positionsArray.push(position);
             this.createRoom(position, sizeX, sizeY, available);
         }
@@ -166,6 +95,7 @@ class Main extends React.Component {
         playerPosition = position;
     }
 
+    // Cheating, not the react way
     updateStats(){
         document.getElementById("hp").innerHTML = health;
         document.getElementById("wp").innerHTML = weapon;
@@ -186,7 +116,6 @@ class Main extends React.Component {
             if(hp <= 0){
                 console.log("Dead");
                 exp += entityStats.att;
-
                 if(expcap <= exp){   // level up
                     exp = exp - expcap, health += 50, weapon += 5, level++, expcap += 5*dungeon;
                     this.updateStats();
@@ -196,11 +125,9 @@ class Main extends React.Component {
                 if(type === "blue"){
                     dungeon++;
                     this.reset();
-                }
-
-                return true;
+                } else return true;
+                
             }
-            
         } else if(type === "purple"){ //items
             health += entityStats.hp;
             document.getElementById("hp").innerHTML = health;
@@ -212,7 +139,9 @@ class Main extends React.Component {
         }
        
         if (health <= 0) {
-            console.log("resetmap");
+            expcap = 10, health = 50, level = 1, exp = 0, weapon = 2, dungeon = 1;
+            this.reset();
+            alert("You died!, start at the beginning peasant!");
         }
         return false;
     }
@@ -246,50 +175,27 @@ class Main extends React.Component {
         this.updateFog(playerPosition);
     }
 
-    renderCreate(length){
-    }
-
     componentDidMount(){
-        // this.setState({dungeon: cells});
         this.forceUpdate();
-        // console.log(this.refs);
+        this.refs.gamefocus.focus();
     }
 
     componentDidUpdate(){
-        console.log("updated");
         this.createMap(gRooms);
-        // console.log(playerPosition);
-        // console.log(playerPosition+1);
-        // console.log(this.refs[playerPosition+1]);
-        // console.log(this.refs[playerPosition+1].fog());
         this.updateFog(playerPosition);
     }
-
-
 
     reset=()=>{
         var length = xLength*yLength;
         while (length--) this.refs[length].reset();
-        // cells = [];
-        // document.getElementById("gameCells").innerHTML =  "";
-
         this.forceUpdate();
-        // cells = [];
-        // this.unmountMe();
-        // this.render();
-        
-        // var length = xLength*yLength;
-        // while (length--) cells[length] = <Cell ref={length} key={length} />;
-        // this.setState(this.Initialstate);
     }
 
     render() {
         var length = xLength*yLength;
-        // console.log(length);
         while (length--) cells[length] = <Cell ref={length} key={length} />;
-        // this.setState({dungeon: cells});
         return (
-            <div className="main" onKeyDown={this.press} tabIndex="0">
+            <div className="main" onKeyDown={this.press} tabIndex="0" ref="gamefocus">
                 <div className="statuses" ref="stats">
                     <button onClick={this.reset}>Reset</button> Health: <span id="hp">{health}</span> Level: <span id="lvl">{level}</span> EXP: <span id="xp">{exp}</span> Weapon: <span id="wp">{weapon}</span> Dungeon: <span id="dng">{dungeon}</span> <button onClick={this.toggleFogofWar}>Toggle Fog Of War</button>
                 </div>
@@ -297,7 +203,6 @@ class Main extends React.Component {
 
                     <div id="gameCells" className="game">
                         {cells}
-                        {/* {this.state.dungeon} */}
                     </div>
                 </div>
             </div>
--- src/scripts/entities.js
+++ src/scripts/entities.js
@@ -3,7 +3,6 @@ import React from "react";
 export class Cell extends React.Component {
     constructor(props) {
         super(props);
-
         this.state = this.InitialState;
     }
 
@@ -25,10 +24,6 @@ export class Cell extends React.Component {
         return {hp: this.state.health, att: this.state.attack};
     }
 
-    getVacancy(){
-        return this.state.entity;
-    }
-
     checkEntity() {
         return this.state.entity;
     }
@@ -48,140 +43,7 @@ export class Cell extends React.Component {
 
     render() {
         return (
-            <div id={this.props.id} className="cell" style={{ backgroundColor: this.state.fog ? "black" : this.state.entity }}>
-
-            </div>
+            <div id={this.props.id} className="cell" style={{ backgroundColor: this.state.fog ? "black" : this.state.entity }}></div>
         )
     };
 }
-
-export class Monster extends React.Component {
-    state = {
-        fog: false
-    };
-
-    shouldComponentUpdate(nextProps, nextState){
-        return this.state.fog !== nextState.fog;
-    }
-
-    render(){
-        return (
-            <div className="cell" style={{backgroundColor: this.state.fog ? "black" : "blue"}}></div>
-        )
-    }
-}
-
-export class Items extends React.Component {
- state = {
-        fog: false
-    };
-
-    shouldComponentUpdate(nextProps, nextState){
-        return this.state.fog !== nextState.fog;
-    }
-
-    render(){
-        return (
-            <div className="cell" style={{backgroundColor: this.state.fog ? "black" : "Green"}}></div>
-        )
-    }
-}
-
-export class Weapons extends React.Component {
-    state = {
-        fog: false
-    };
-
-    shouldComponentUpdate(nextProps, nextState){
-        return this.state.fog !== nextState.fog;
-    }
-
-    render(){
-        return (
-            <div className="cell" style={{backgroundColor: this.state.fog ? "black" : "yellow"}}></div>
-        )
-    }
-}
-
-export class Boss extends React.Component {
-
- state = {
-        fog: false
-    };
-
-    shouldComponentUpdate(nextProps, nextState){
-        return this.state.fog !== nextState.fog;
-    }
-
-    render(){
-        return (
-            <div className="cell" style={{backgroundColor: this.state.fog ? "black" : "purple"}}></div>
-        )
-    }
-}
-
-export class Player extends React.Component {
-
-
-    // shouldComponentUpdate(nextProps, nextState){
-    //     return this.state.fog !== nextState.fog;
-    // }
-
-    render(){
-        return (
-            <div className="cell" style={{backgroundColor: "red"}}></div>
-        )
-    }
-}
-
-export class Wall extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            fog: props.f
-        }
-    }
-
-    shouldComponentUpdate(nextProps, nextState){
-        // console.log(nextProps);
-        
-        // console.log(nextState);
-        return this.state.fog !== nextProps;
-    }
-
-    componentDidUpdate(prevProps, prevState){
-        // console.log(prevProps);
-        
-        // console.log(prevState);
-        // console.log("Updated");
-    }
-
-    hello = () => {
-        console.log("Hello from outer space");
-    }
-
-    fog = () => {
-        this.setState({fog: !this.state.fog});
-    }
-
-    render(){
-        return (
-            <div className="cell" ref={this.props.reference} style={{backgroundColor: this.state.fog ? "black" : "grey"}}></div>
-        )
-    }
-}
-
-export class Road extends React.Component {
-    render(){
-        return (<div className="cell" style={{backgroundColor: "white"}}></div>)
-    }
-}
-
-// module.exports = {
-//     monster: Monster,
-//     item: Items,
-//     boss: Boss,
-//     weapon: Weapons,
-//     player: Player,
-//     wall: Wall
-// }
